b15cd60c2ce37acc2262f20074b078aba238ea16
Dylan <40590952+TheDjgamerProductions@users.noreply.github.com>
2022-02-17 09:16:05 +1100

    Added Toggle LED

---
+++ Push_Button_Toggle_LED/Push_Button_Toggle_LED.ino
@@ -0,0 +1,32 @@
+bool Toggle = false;
+int i = 0;
+  
+  
+  
+  
+void setup()
+{
+  pinMode(2, INPUT);
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  if (digitalRead(2) == HIGH) {
+    Toggle = !Toggle;
+    Serial.println(Toggle);
+    while (digitalRead(2) == HIGH) { //Supporting code to prevent weird behaviour with bounce and when the user holds down the button
+    }
+  }
+  switch (Toggle) {
+    
+    case false:
+    	digitalWrite(13, LOW);
+    	break;
+    case true:    
+    	digitalWrite(13, HIGH);
+    	break;
+  }
+  delay(100);
+}
