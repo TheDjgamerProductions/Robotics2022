0ecdcfd0f70181851c7fc98b98b8764416383bb2
Zefffie <dylanjp@iinet.net.au>
2022-03-03 09:40:09 +1100

    Updated Pot Pin Array With LEDs

--- Practical Exercises/Week 5/pot_avrage_array/pot_avrage_array.ino
+++ Practical Exercises/Week 5/pot_avrage_array/pot_avrage_array.ino
@@ -1,12 +1,16 @@
 int PotPins[] = {A0,A1,A2,A3,A4};
-int PinsSize = sizeof(PotPins) / sizeof(int);
+int LEDPins[] = {2,3,4,5,6};
+int PotPinsSize = sizeof(PotPins) / sizeof(int);
+int LEDPinsSize = sizeof(LEDPins) / sizeof(int);
 int PotReadout[5];
 
 void setup() {
  Serial.begin(9600);
- Serial.println(PinsSize);
- for (int i = 0; i < PinsSize; i++)   {
+ for (int i = 0; i < PotPinsSize; i++)   {
     pinMode(PotPins[i], INPUT);
+  }
+ for (int i = 0; i < LEDPinsSize; i++)   {
+    pinMode(LEDPins[i], OUTPUT);
   }
 }
 
@@ -14,27 +18,39 @@ void loop() {
   
   readPins();
   avrage();
-  
+  updateLED();
 }
 
 
 void readPins() {
-    for (int i = 0; i < PinsSize; i++)   {
+    for (int i = 0; i < PotPinsSize; i++)   {
       PotReadout[i] = analogRead(PotPins[i]);
-      //Serial.println(PotReadout[i]);
       delay(50);
   }
 }
 
 int avrage() {
   int avragePot = 0;
-  for (int i = 0; i < PinsSize; i++)   {
+  for (int i = 0; i < PotPinsSize; i++)   {
   avragePot = avragePot + PotReadout[i];
     Serial.print(PotReadout[i]);
     Serial.print(",");
   }  
-  avragePot = avragePot/PinsSize;
+  avragePot = avragePot/PotPinsSize;
   Serial.print("The avrage is: ");
   Serial.println(avragePot);
+  
+}
+
+void updateLED() {
+  for (int i = 0; i < PotPinsSize; i++) {
+    int x = map(PotReadout[i], 0, 1023, 0, 255);
+    analogWrite(LEDPins[i], x);
+    
+    
+  }
+  
+ 
+  
   
 }
