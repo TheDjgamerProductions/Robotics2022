b0e646bb8b45ecc7e00b800238ee52bf2ce74ace
Zefffie <dylanjp@iinet.net.au>
2022-05-24 12:44:51 +1000

    Defined Pins

--- Assessments/Assessment_2_Smart_Device_House/Assessment_2_Smart_Device_House.ino
+++ Assessments/Assessment_2_Smart_Device_House/Assessment_2_Smart_Device_House.ino
@@ -1,3 +1,26 @@
+#define ServoPin = 22
+#define mDrive1 = 7
+#define mDrive2 = 6
+#define potPin = A8
+#define sdSelect = 41
+#define blueSelect = 10
+#define redLED = 31
+#define YellLED = 30
+#define greenLED = 32
+#define line = 49
+#define button = 3
+#define echo = 4
+#define Trig = 5
+#define buzzer = 40
+
+
+
+
+
+
+
+
+
 void setup() {
 
 
