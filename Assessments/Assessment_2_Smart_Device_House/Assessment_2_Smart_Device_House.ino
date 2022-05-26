7a666c28bcd3a0fe77131b0ac58e6f287e3c805d
Dylan <dylanjp@iinet.net.au>
2022-05-26 11:04:02 +1000

    Update Assessment_2_Smart_Device_House.ino

--- Assessments/Assessment_2_Smart_Device_House/Assessment_2_Smart_Device_House.ino
+++ Assessments/Assessment_2_Smart_Device_House/Assessment_2_Smart_Device_House.ino
@@ -1,17 +1,28 @@
-#define ServoPin = 22
-#define mDrive1 = 7
-#define mDrive2 = 6
-#define potPin = A8
-#define sdSelect = 41
-#define blueSelect = 10
-#define redLED = 31
-#define YellLED = 30
-#define greenLED = 32
-#define line = 49
-#define button = 3
-#define echo = 4
-#define Trig = 5
-#define buzzer = 40
+// SD Card Module
+#include <SPI.h>
+#include <SD.h>
+
+// Real Time Clock (RTC)
+#include "RTClib.h"
+RTC_Millis rtc;     // Software Real Time Clock (RTC)
+DateTime rightNow;  // used to store the current time.
+
+#define ServoPin  22
+#define mDrive1  7
+#define mDrive2  6
+#define potPin  A8
+#define sdSelect  41
+#define ADAFRUITBLE_REQ 10
+#define ADAFRUITBLE_RDY  2
+#define ADAFRUITBLE_RST  9
+#define redLED  31
+#define YellLED  30
+#define greenLED 32
+#define line  49
+#define button  3
+#define echo  4
+#define Trig  5
+#define buzzer  40
 
 
 
@@ -22,10 +33,75 @@
 
 
 void setup() {
-
+  Serial.begin(9600);           // Open serial communications and wait for port to open:
+  while (!Serial) {
+    delay(1);                   // wait for serial port to connect. Needed for native USB port only
+  }
+  pinMode(53, OUTPUT);
+// SD Card initialisation
+  Serial.print("Initializing SD card...");
+  if (!SD.begin(sdSelect)) {
+    Serial.println("initialization failed!");
+    while (1);
+  }
+// Real Time Clock (RTC)
+  rtc.begin(DateTime(F(__DATE__), F(__TIME__)));
+  Serial.println("initialization done.");
+  logEvent("System Initialisation...");
 
 }
 
 void loop() {
 
+}
+
+
+
+
+void logEvent(String dataToLog) {
+  /*
+     Log entries to a file on an SD card.
+  */
+  // Get the updated/current time
+  DateTime rightNow = rtc.now();
+
+  // Open the log file
+  File logFile = SD.open("events.csv", FILE_WRITE);
+  if (!logFile) {
+    Serial.print("Couldn't create log file");
+    abort();
+  }
+
+  // Log the event with the date, time and data
+  logFile.print(rightNow.year(), DEC);
+  logFile.print(",");
+  logFile.print(rightNow.month(), DEC);
+  logFile.print(",");
+  logFile.print(rightNow.day(), DEC);
+  logFile.print(",");
+  logFile.print(rightNow.hour(), DEC);
+  logFile.print(",");
+  logFile.print(rightNow.minute(), DEC);
+  logFile.print(",");
+  logFile.print(rightNow.second(), DEC);
+  logFile.print(",");
+  logFile.print(dataToLog);
+
+  // End the line with a return character.
+  logFile.println();
+  logFile.close();
+  Serial.print("Event Logged: ");
+  Serial.print(rightNow.year(), DEC);
+  Serial.print(",");
+  Serial.print(rightNow.month(), DEC);
+  Serial.print(",");
+  Serial.print(rightNow.day(), DEC);
+  Serial.print(",");
+  Serial.print(rightNow.hour(), DEC);
+  Serial.print(",");
+  Serial.print(rightNow.minute(), DEC);
+  Serial.print(",");
+  Serial.print(rightNow.second(), DEC);
+  Serial.print(",");
+  Serial.println(dataToLog);
 }
