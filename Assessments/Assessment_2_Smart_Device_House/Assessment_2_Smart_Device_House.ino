f937d5de55be5e37e34fc8d756695470ba3a2ebd
Zefffie <dylanjp@iinet.net.au>
2022-06-02 10:30:42 +1000

    Update Assessment_2_Smart_Device_House.ino

--- Assessments/Assessment_2_Smart_Device_House/Assessment_2_Smart_Device_House.ino
+++ Assessments/Assessment_2_Smart_Device_House/Assessment_2_Smart_Device_House.ino
@@ -5,13 +5,9 @@
 #include "RTClib.h"
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
-static const int RXPin = 10, TXPin = 11;
-static const uint32_t GPSBaud = 9600;
 
-// The TinyGPS++ object
-TinyGPSPlus gps;
-// The serial connection to the GPS device
-SoftwareSerial ss(RXPin, TXPin);
+
+
 
 
 
@@ -27,7 +23,7 @@ DateTime rightNow;  // used to store the current time.
 #define ServoPin  24
 #define mDrive1  7
 #define mDrive2  6
-#define potPin  A8
+#define potPin  "A8"
 #define sdSelect  41
 #define redLED  31
 #define YellLED  30
@@ -41,6 +37,22 @@ DateTime rightNow;  // used to store the current time.
 //Defines
 #define TrigDistance 100
 
+//Vars
+const char *gpsStream =
+  "$GPRMC,045103.000,A,3014.1984,N,09749.2872,W,0.67,161.46,030913,,,A*7C\r\n"
+  "$GPGGA,045104.000,3014.1985,N,09749.2873,W,1,09,1.2,211.6,M,-22.5,M,,0000*62\r\n"
+  "$GPRMC,045200.000,A,3014.3820,N,09748.9514,W,36.88,65.02,030913,,,A*77\r\n"
+  "$GPGGA,045201.000,3014.3864,N,09748.9411,W,1,10,1.2,200.8,M,-22.5,M,,0000*6C\r\n"
+  "$GPRMC,045251.000,A,3014.4275,N,09749.0626,W,0.51,217.94,030913,,,A*7D\r\n"
+  "$GPGGA,045252.000,3014.4273,N,09749.0628,W,1,09,1.3,206.9,M,-22.5,M,,0000*6F\r\n";\
+  static const int RXPin = 10, TXPin = 11;
+  static const uint32_t GPSBaud = 9600;
+
+
+// The TinyGPS++ object
+TinyGPSPlus gps;
+// The serial connection to the GPS device
+SoftwareSerial ss(RXPin, TXPin);
 
 void setup() {
   //Setup pins
@@ -56,16 +68,19 @@ void setup() {
   pinMode(Trig, OUTPUT);
   pinMode(echo, INPUT);
   servo.attach(ServoPin);       //Set up servo
-  Serial.begin(9600);           // Open serial communications and wait for port to open:
+  Serial.begin(115200);           // Open serial communications and wait for port to open:
+  ss.begin(GPSBaud);
   while (!Serial) {
     delay(1);                   // wait for serial port to connect. Needed for native USB port only
   }
-  ss.begin(GPSBaud);
   Serial.print("Initializing SD card...");
   if (!SD.begin(sdSelect)) {
     Serial.println("initialization failed!");
     while (1);
   }
+  while (*gpsStream)
+    if (gps.encode(*gpsStream++))
+      getGPSInfo();
 // Real Time Clock (RTC)
   rtc.begin(DateTime(F(__DATE__), F(__TIME__)));
   Serial.println("initialization done.");
@@ -73,11 +88,19 @@ logEvent("System Initialisation...");
 }
 
 void loop() {
-
+  Serial.println("In main loop");
+  stateHandeler();
+  getGPSInfo();
+  delay(50);
 }
 
 
 
+/*
+ * 
+ * @pram string (What data to log)
+ * @return null
+ */
 
 void logEvent(String dataToLog) {
   /*
@@ -127,6 +150,11 @@ void logEvent(String dataToLog) {
   Serial.println(dataToLog);
 }
 
+/*
+ * 
+ * @pram null
+ * @return boolean
+ */
 boolean Read_distance() {
   long duration; 
   int distance;
@@ -147,4 +175,108 @@ boolean Read_distance() {
   else {
     return(false);
   }
+}
+
+/*
+ * This function is used to make the house in an armed state if the GPS is outside the house
+ * @pram null
+ * @return null
+ */
+void stateHandeler(){
+
+
+
+}
+
+/*
+ * 
+ * @pram null
+ * @return null
+ */
+void disarmedState(){
+
+
+
+}
+
+/*
+ * 
+ * @pram null
+ * @return null
+ */
+void armedState(){
+
+
+
+}
+
+/*
+ * 
+ * @pram null
+ * @return null
+ */
+void ledController(){
+
+
+}
+
+/*
+ * 
+ * @pram null
+ * @return null
+ */
+void fanController(){
+
+  
+}
+
+void getGPSInfo()
+{
+  Serial.print(F("Location: ")); 
+  if (gps.location.isValid())
+  {
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(F(","));
+    Serial.print(gps.location.lng(), 6);
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F("  Date/Time: "));
+  if (gps.date.isValid())
+  {
+    Serial.print(gps.date.month());
+    Serial.print(F("/"));
+    Serial.print(gps.date.day());
+    Serial.print(F("/"));
+    Serial.print(gps.date.year());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F(" "));
+  if (gps.time.isValid())
+  {
+    if (gps.time.hour() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.hour());
+    Serial.print(F(":"));
+    if (gps.time.minute() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.minute());
+    Serial.print(F(":"));
+    if (gps.time.second() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.second());
+    Serial.print(F("."));
+    if (gps.time.centisecond() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.centisecond());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.println();
 }
